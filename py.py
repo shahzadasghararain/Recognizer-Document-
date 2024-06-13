c9b25d4eb825d5c60eadf024459277b310325c5e
shahzadasghararain <161644439+shahzadasghararain@users.noreply.github.com>
2024-06-13 23:24:11 +0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,64 @@
+
+
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient
+
+
+endpoint = "https://shahzad123.cognitiveservices.azure.com/"
+key = "key"
+
+def format_bounding_box(bounding_box):
+    if not bounding_box:
+        return "N/A"
+    return ", ".join(["[{}, {}]".format(p.x, p.y) for p in bounding_box])
+
+def analyze_read():
+    # sample document
+    formUrl = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/sample-layout.pdf"
+
+    document_analysis_client = DocumentAnalysisClient(
+        endpoint=endpoint, credential=AzureKeyCredential(key)
+    )
+    
+    poller = document_analysis_client.begin_analyze_document_from_url(
+            "prebuilt-read", formUrl)
+    result = poller.result()
+
+    print ("Document contains content: ", result.content)
+    
+    for idx, style in enumerate(result.styles):
+        print(
+            "Document contains {} content".format(
+                "handwritten" if style.is_handwritten else "no handwritten"
+            )
+        )
+
+    for page in result.pages:
+        print("----Analyzing Read from page #{}----".format(page.page_number))
+        print(
+            "Page has width: {} and height: {}, measured with unit: {}".format(
+                page.width, page.height, page.unit
+            )
+        )
+
+        for line_idx, line in enumerate(page.lines):
+            print(
+                "...Line # {} has text content '{}' within bounding box '{}'".format(
+                    line_idx,
+                    line.content,
+                    format_bounding_box(line.polygon),
+                )
+            )
+
+        for word in page.words:
+            print(
+                "...Word '{}' has a confidence of {}".format(
+                    word.content, word.confidence
+                )
+            )
+
+    print("----------------------------------------")
+
+
+if __name__ == "__main__":
+    analyze_read()
